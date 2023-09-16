e0a859532b0f340158988133df00459fddf39aa8
Jose <eulzmagnaye@gmail.com>
2023-09-16 18:08:31 +0800

    refactor code

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,20 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+
 export {
     Hero,
     CustomButton,
     Footer,
-    Navbar
+    Navbar,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+    CarCard,
+    ShowMore
 }
